2aa3ce67093371aeb51db2d82f420a5bc3047208
samuelosei <s.osei40@yahoo.com>
2020-03-01 22:45:15 -0500

    update

--- src/test/java/com/atomation/test/day1/MyFirstSeleniumScript.java
+++ src/test/java/com/atomation/test/day1/MyFirstSeleniumScript.java
@@ -8,6 +8,9 @@ public class MyFirstSeleniumScript {
     public static void main(String[] args) {
         //Three(3) this to do
         //1.set chromedriver
+        //2.create chromedriver
+        //3.open some website
+        //1.set chromedriver
         WebDriverManager.chromedriver().setup();
         //2.create chromedriver object
         ChromeDriver driver = new ChromeDriver();
